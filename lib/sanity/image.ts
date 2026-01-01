879f58d21f1d4a1688cc73ee3e6e2bcc78af1270
Bethelem Melese <bethelem.m@ambftradingplc.com>
2026-01-01 11:57:48 +0300

    fix image display issue

--- lib/sanity/image.ts
+++ lib/sanity/image.ts
@@ -1,9 +1,18 @@
 import imageUrlBuilder from '@sanity/image-url'
 import { client } from './client'
 import { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import type { ImageUrlBuilder } from '@sanity/image-url/lib/types/builder'
 
-const builder = imageUrlBuilder(client)
+let _builder: ImageUrlBuilder | null = null
+
+function getBuilder(): ImageUrlBuilder {
+  if (!_builder) {
+    _builder = imageUrlBuilder(client)
+  }
+  return _builder
+}
 
 export function urlFor(source: SanityImageSource) {
+  const builder = getBuilder()
   return builder.image(source)
 }
